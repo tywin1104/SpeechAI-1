77816bc70a6ee3825d45605ceb4a51bef262c02a
Tianyi Zhang <tianyizhang@dhcp-206-12-54-46.eduroam.wireless.ubc.ca>
2018-01-14 01:37:55 -0800

    community function

---
+++ SpeechAI/FeedsViewController.swift
@@ -0,0 +1,65 @@
+//
+//  FeedsViewController.swift
+//  
+//
+//  Created by Tianyi Zhang on 2018-01-14.
+//
+
+import UIKit
+import AVFoundation
+
+class FeedsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var audioPlayer: AVAudioPlayer?
+
+    @IBAction func playAudio(_ sender: Any) {
+        let path = Bundle.main.path(forResource: "example.mp3", ofType:nil)!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.play()
+        } catch {
+            print("Error!!!")
+        }
+        
+    }
+    @IBAction func commentPressed(_ sender: Any) {
+        
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 25
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
