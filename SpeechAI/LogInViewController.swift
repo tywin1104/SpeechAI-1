f347d432eca32210c74f81632530921a1eb160b8
TheCurryMan <avinashkalpitjain@gmail.com>
2018-01-16 23:32:53 -0800

    Login functional

--- SpeechAI/LogInViewController.swift
+++ SpeechAI/LogInViewController.swift
@@ -7,14 +7,19 @@
 //
 
 import UIKit
+import Firebase
 
 class LogInViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.backgroundColor = UIColor(gradientStyle: .topToBottom, withFrame: self.view.frame, andColors: [#colorLiteral(red: 0.1647058824, green: 0.9607843137, blue: 0.5960784314, alpha: 1), #colorLiteral(red: 0.03137254902, green: 0.6823529412, blue: 0.9176470588, alpha: 1)])
-        // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,8 +27,54 @@ class LogInViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    @IBAction func loginUser(_ sender: Any) {
+        
+    }
+    
     @IBAction func dismissView(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func tappedScreen(_ sender: Any) {
+        print("tapped")
+        self.emailField.resignFirstResponder()
+        self.passwordField.resignFirstResponder()
+    }
+    
+    @IBAction func pressedSignIn(_ sender: Any) {
+        print("pressed")
+        Auth.auth().signIn(withEmail: self.emailField.text!, password: self.passwordField.text!) { (user, error) in
+            if user != nil {
+                print("sign in successful")
+                User.currentUser.setUpUser()
+                
+                self.performSegue(withIdentifier: "login", sender: nil)
+            }
+            else{
+                print("sign in failed")
+                
+            }
+        }
+        
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0{
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+        
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y != 0{
+                self.view.frame.origin.y += keyboardSize.height
+            }
+        }
     }
     
     /*
